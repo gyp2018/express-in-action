fabc53965b635b6777c5e8a22989ec1c4ec37689
GimYoungPhil <youngphil.gim@gmail.com>
2018-04-07 01:18:34 +0900

    add ch1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Express in Action
+
+```sh
+$ node --version
+v8.9.1
+
+$ npm --version
+5.5.1
+
+$ yarn --version
+1.5.1
+```
---
+++ src/ch1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, function() {
+  console.log('Express app started on port 3000.');
+});
